2972134fc77792048b6d25415b78edca713b151a
charit nanduri <charithnanduri03@gmail.com>
2021-11-08 21:23:22 +0000

    add app.get to output json doc

--- express_server.js
+++ express_server.js
@@ -2,9 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+const urlDatabase = {
+  "one": 'http://lighthouselabs.ca',
+  "two": 'http://www.google.com'
+}
+
 app.get('/',(req,res) => {
   res.send("Hello WOrld!")
 });
+
+app.get('/urls.json',(req,res) => {
+  res.json(urlDatabase);
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);

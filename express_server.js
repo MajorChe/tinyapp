97fe4654fe7e49b76332eaa0819355832353a139
charit nanduri <charithnanduri03@gmail.com>
2021-11-09 22:05:17 +0000

    add delete route

--- express_server.js
+++ express_server.js
@@ -39,13 +39,22 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get('/urls/:shortURL',(req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
-  res.render('urls_show',templateVars);
+  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  if(!templateVars.longURL) {
+    res.redirect('/urls');
+  } else{
+    res.render('urls_show',templateVars);
+  }
 });
 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+});
+
+app.post("/urls/:shortURL/delete", (req,res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls')
 });
 
 
